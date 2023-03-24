4e10cbbf19128dafe0da609aa3c7e6e350069ce5
wumeng <2100172067@qq.com>
2023-03-24 19:53:03 +0800

    one

---
+++ study/1.ioc.ts
@@ -0,0 +1,23 @@
+/*
+ * @Author: wumeng 2100172067@qq.com
+ * @Date: 2023-03-24 19:45:36
+ * @LastEditors: wumeng 2100172067@qq.com
+ * @LastEditTime: 2023-03-24 19:51:30
+ * @FilePath: \JavaScript\Nest\study\1.ioc.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+class A{
+    name:string
+    constructor(name:string)
+    {
+        this.name=name
+    }
+}
+
+class B{
+    name:string
+    constructor(name:string)
+    {
+        this.name=name
+    }
+}
